99a204d7c5f010d7b3401a3820ad2f7bcd3667e6
Quique Gómez <kikebmg@gmail.com>
2021-02-12 17:15:06 +0100

    fix paths

--- php/pages/formularioInsertar.php
+++ php/pages/formularioInsertar.php
@@ -7,19 +7,19 @@
 	
 	<title>Nuevo</title>
 	<!--	CSS		-->
-	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css">
-	<link rel="stylesheet" href="../css/main.css" type="text/css">
-	<link href="../css/star-rating.min.css" media="all" rel="stylesheet" type="text/css"/>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" href="../../css/main.css" type="text/css">
+	<link href="../../resources/plugins/star-rating/css/main.css" media="all" rel="stylesheet" type="text/css"/>
 	<!--	JavaScripts Bootstrap y jQuery		-->
-	<script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
-	<script src="../js/popper.min.js" type="text/javascript"></script>
-	<script src="../js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
 	<!--	JavaScripts propios y Plugin estrella		-->
-	<script src="../js/star-rating.js" type="text/javascript"></script>
-	<script src="../js/mostrarComic.js" type="text/javascript"></script>
-	<script src="../js/formularioInsertar.js" type="text/javascript"></script>
+	<script src="../../js/star-rating.js" type="text/javascript"></script>
+	<script src="../../js/mostrarComic.js" type="text/javascript"></script>
+	<script src="../../js/formularioInsertar.js" type="text/javascript"></script>
 	<!--	Textos y traducciones de las estrellas		-->
-	<script src="../js/locales/es.js" type="text/javascript"></script>
+	<script src="../../resources/plugins/star-rating/locales/es.js" type="text/javascript"></script>
 </head>
 <body>
 	<!--  Barra de Navegación	-->
