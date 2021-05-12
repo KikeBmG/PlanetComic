c88764cb87a7ce1ab7e016f23de714c00be2d86d
Quique Gómez <kikebmg@gmail.com>
2021-05-12 13:29:39 +0200

    feat(): node implementation 2

--- js/comicsGuionista.js
+++ js/comicsGuionista.js
@@ -1,5 +1,7 @@
+import { launchQuery } from '../php/connection';
+
 let calculate_age = dob => {
-	//param dob =  date of birthday
+	//param dob =>  date of birthday
 	let fechaNac = dob.split('-');
 	let formattedFecha = new Date(fechaNac[2], fechaNac[1], fechaNac[0]);
 
@@ -12,100 +14,94 @@ let calculate_age = dob => {
 $(document).ready(() => {
 	let idGuionista = $('span.invisible').attr('id');
 
-	$.getJSON('datosGuionista.php?idGuionista=' + idGuionista, data => {
-		let age = calculate_age(data[0].fechaNacGuionista);
-		let aux = '';
+	let getDatosGuionista = `SELECT idGuionista, nombreGuionista, nacionalidadGuionista, fotoGuionista, fechaNacGuionista FROM guionista WHERE idGuionista = '${idGuionista}'`;
+	let datosGuionista = launchQuery(getDatosGuionista);
 
-		aux += `<div class="row">` //abre fila total
+	let age = calculate_age(datosGuionista[0].fechaNacGuionista);
+	let aux = '';
 
-		aux += `<div class="col-sm-3">`; //abre columna foto
+	aux += `<div class="row">` //abre fila total
 
-		aux += `<img src="../resources/img/guionistas/${data[0].fotoGuionista}" width="240px" height="285px">`;
+	aux += `<div class="col-sm-3">`; //abre columna foto
 
-		aux += `</div>`; //cierra columna foto
+	aux += `<img src="../resources/img/guionistas/${datosGuionista[0].fotoGuionista}" width="240px" height="285px">`;
 
-		aux += `<div class="col-sm-6">`; //abre columna datos
+	aux += `</div>`; //cierra columna foto
 
-		aux += `<div class="row">`; //abre fila nombre
+	aux += `<div class="col-sm-6">`; //abre columna datos
 
-		aux += `<h4 class="text-white">${data[0].nombreGuionista}</h4>`;
+	aux += `<div class="row">`; //abre fila nombre
 
-		aux += `</div>`; //cierra fila nombre
+	aux += `<h4 class="text-white">${datosGuionista[0].nombreGuionista}</h4>`;
 
-		aux += `<div class="row">`; //abre fila edad y fecha nacimiento
+	aux += `</div>`; //cierra fila nombre
 
-		aux += `<h4 class="text-white">${data[0].fechaNacGuionista} (${age} años)</h4>`;
+	aux += `<div class="row">`; //abre fila edad y fecha nacimiento
 
-		aux += `</div>`; //cierra fila edad y fecha nacimiento
+	aux += `<h4 class="text-white">${datosGuionista[0].fechaNacGuionista} (${age} años)</h4>`;
 
-		aux += `<div class="row">`; //abre fila nacionalidad
+	aux += `</div>`; //cierra fila edad y fecha nacimiento
 
-		aux += `<h4 class="text-white">${data[0].nacionalidadGuionista}</h4>`;
+	aux += `<div class="row">`; //abre fila nacionalidad
 
-		aux += `</div>`; //cierra fila nacionalidad
+	aux += `<h4 class="text-white">${datosGuionista[0].nacionalidadGuionista}</h4>`;
 
-		aux += `<div class="row">`; //abre fila karma
+	aux += `</div>`; //cierra fila nacionalidad
 
-		aux += `<button class="btn btn-success my-2 my-sm-0 margen-derecho-1 votakarma" onClick="${votaKarmaGuionista(idGuionista, (+1))}"><img src="../resources/img/iconos/glyphicons-344-thumbs-up.png" height="16px" alt="positivo"></button>`; //boton subir karma
+	aux += `<div class="row">`; //abre fila karma
 
-		aux += `<button class="btn btn-danger my-2 my-sm-0 votakarma" onClick="${votaKarmaGuionista(idGuionista, (-1))}"><img src="../resources/img/iconos/glyphicons-345-thumbs-down.png" height="16px" alt="negativo"></button>`; //boton bajar karma
+	aux += `<button class="btn btn-success my-2 my-sm-0 margen-derecho-1 votakarma" onClick="${votaKarmaGuionista(idGuionista, (+1))}"><img src="../resources/img/iconos/glyphicons-344-thumbs-up.png" height="16px" alt="positivo"></button>`; //boton subir karma
 
-		aux += `<span class="none texto-blanco margen-izquierdo-3" id="feedbackKarmaGuionista">¡Gracias por votar!</span>`;
+	aux += `<button class="btn btn-danger my-2 my-sm-0 votakarma" onClick="${votaKarmaGuionista(idGuionista, (-1))}"><img src="../resources/img/iconos/glyphicons-345-thumbs-down.png" height="16px" alt="negativo"></button>`; //boton bajar karma
 
-		aux += `</div>`; //cierra fila karma
+	aux += `<span class="none texto-blanco margen-izquierdo-3" id="feedbackKarmaGuionista">¡Gracias por votar!</span>`;
 
-		aux += `</div>`; //cierra columna datos
+	aux += `</div>`; //cierra fila karma
 
-		aux += `</div>`; //cierra fila total
+	aux += `</div>`; //cierra columna datos
 
-		aux += `<div class="row"><h1 class="col-lg-11 text-white center versalitas">Comics</h1></div>`;
+	aux += `</div>`; //cierra fila total
 
-		$('#datos').html(aux);
-	});
+	aux += `<div class="row"><h1 class="col-lg-11 text-white center versalitas">Comics</h1></div>`;
 
-	$.getJSON('comicsGuionista.php?idGuionista=' + idGuionista, data => {
-		let cont = 0;
-		let aux = '';
+	$('#datos').html(aux);
 
-		aux += `<div class="row">`;//inicio div fila
+	let getComicsGuionista = `SELECT DISTINCT portada, nombreComic, idComic FROM comic WHERE idGuionista = '${idGuionista}'`;
+	let comicsGuionista = launchQuery(getComicsGuionista);
+	let cont = 0;
+	let aux2 = '';
 
-		for (let i = 0; i < data.length; i++) {
-			if (cont == 3) {
-				aux += `</div>`; // cierra div row
-				aux += `<div class="row">`;//inicio de nuevo div fila
-				cont = 0; //resetea el contador
-			}
-			//aux += `<div class='col-sm-4'>`; //dentro del div contenedor del item inicio un div columna
-			aux += `<div class="card bg-dark text-white" style="width: 20rem;">`; //dentro del div columna inicio un div card
+	aux2 += `<div class="row">`;//inicio div fila
 
-			aux += `<img src="../resources/img/portadas/${data[i].portada}" class="card-img-top" width="320px" height="500px">`; //imagen dentro del div item
-			aux += `<div class="card-block">`; // abre card-block
-			aux += `<h4 class="card-title">${data[i].nombreComic}</h4>`;
-			aux += `<a class="btn btn-primary align-items-end" href="#myModal" onClick="${modalDatosComic(data[i].idComic)}" data-toggle="modal" data-target=".bd-example-modal-lg">Ver detalles</a>`;
-
-			aux += `</div>`; //cierra div card-block
-			aux += `</div>`; //cierra div card
-			//aux += `</div>`; //cierra div columna
-			cont++;
+	for (let i = 0; i < comicsGuionista.length; i++) {
+		if (cont == 3) {
+			aux2 += `</div>`; // cierra div row
+			aux2 += `<div class="row">`;//inicio de nuevo div fila
+			cont = 0; //resetea el contador
 		}
-		aux += `</div>`; //cierra div fila
 
-		$('#comics').html(aux);
-	});
+		aux2 += `<div class="card bg-dark text-white" style="width: 20rem;">`; //dentro del div columna inicio un div card
+
+		aux2 += `<img src="../resources/img/portadas/${comicsGuionista[i].portada}" class="card-img-top" width="320px" height="500px">`; //imagen dentro del div item
+		aux2 += `<div class="card-block">`; // abre card-block
+		aux2 += `<h4 class="card-title">${comicsGuionista[i].nombreComic}</h4>`;
+		aux2 += `<a class="btn btn-primary align-items-end" href="#myModal" onClick="${modalDatosComic(comicsGuionista[i].idComic)}" data-toggle="modal" data-target=".bd-example-modal-lg">Ver detalles</a>`;
+
+		aux2 += `</div>`; //cierra div card-block
+		aux2 += `</div>`; //cierra div card
+		cont++;
+	}
+	aux2 += `</div>`; //cierra div fila
+
+	$('#comics').html(aux2);
+
 });
 
 let votaKarmaGuionista = (idGuionista, karma) => {
-	$.ajax({
-		type: 'POST',
-		url: '../php/votarKarmaGuionista.php',
-		data: {
-			idGuionista: idGuionista,
-			karma: karma
-		},
-		success: data => {
-			$('.votakarma').prop('disabled', true);
-			$('#feedbackKarmaGuionista').removeClass('none');
-			$('#feedbackKarmaGuionista').addClass('block');
-		}
-	});
+	let updateKarmaGuionista = `UPDATE guionista SET karmaGuionista = karmaGuionista + ${karma} WHERE idGuionista = '${idGuionista}'`;
+	launchQuery(updateKarmaGuionista);
+
+	$('.votakarma').prop('disabled', true);
+	$('#feedbackKarmaGuionista').removeClass('none');
+	$('#feedbackKarmaGuionista').addClass('block');
 }
--- js/comicsIlustrador.js
+++ js/comicsIlustrador.js
@@ -1,3 +1,5 @@
+import { launchQuery } from '../php/connection';
+
 let calculate_age = dob => {
 	//param dob =  date of birthday
 	let fechaNac = dob.split('-');
@@ -12,101 +14,95 @@ let calculate_age = dob => {
 $(document).ready(() => {
 	let idIlustrador = $('span.invisible').attr('id');
 
-	$.getJSON('datosIlustrador.php?idIlustrador=' + idIlustrador, data => {
-		let aux = '';
+	let getDatosIlustrador = `SELECT idIlustrador, nombreIlustrador, nacionalidadIlustrador, fotoIlustrador, fechaNacIlustrador FROM ilustrador WHERE idIlustrador = '${idIlustrador}'`;
+	let datosIlustrador = launchQuery(getDatosIlustrador);
 
-		aux += `<div class="row">`; //abre fila total
+	let aux = '';
 
-		aux += `<div class="col-sm-3">`; //abre columna foto
+	aux += `<div class="row">`; //abre fila total
 
-		aux += `<img src="../resources/img/ilustradores/${data[0].fotoIlustrador}" width="240px" height="285px">`;
+	aux += `<div class="col-sm-3">`; //abre columna foto
 
-		aux += `</div>`; //cierra columna foto
+	aux += `<img src="../resources/img/ilustradores/${datosIlustrador[0].fotoIlustrador}" width="240px" height="285px">`;
 
-		aux += `<div class="col-sm-6">`; //abre columna datos
+	aux += `</div>`; //cierra columna foto
 
-		aux += `<div class="row">`; //abre fila nombre
+	aux += `<div class="col-sm-6">`; //abre columna datos
 
-		aux += `<h4 class="text-white">${data[0].nombreIlustrador}</h4>`;
+	aux += `<div class="row">`; //abre fila nombre
 
-		aux += `</div>`; //cierra fila nombre
+	aux += `<h4 class="text-white">${datosIlustrador[0].nombreIlustrador}</h4>`;
 
-		aux += `<div class="row">`; //abre fila edad y fecha nacimiento
+	aux += `</div>`; //cierra fila nombre
 
-		let age = calculate_age(data[0].fechaNacIlustrador);
+	aux += `<div class="row">`; //abre fila edad y fecha nacimiento
 
-		aux += `<h4 class="text-white">${data[0].fechaNacIlustrador} (${age} años)</h4>`;
+	let age = calculate_age(datosIlustrador[0].fechaNacIlustrador);
 
-		aux += `</div>`; //cierra fila edad y fecha nacimiento
+	aux += `<h4 class="text-white">${datosIlustrador[0].fechaNacIlustrador} (${age} años)</h4>`;
 
-		aux += `<div class="row">`; //abre fila nacionalidad
+	aux += `</div>`; //cierra fila edad y fecha nacimiento
 
-		aux += `<h4 class="text-white">${data[0].nacionalidadIlustrador}</h4>`;
+	aux += `<div class="row">`; //abre fila nacionalidad
 
-		aux += `</div>`; //cierra fila nacionalidad
+	aux += `<h4 class="text-white">${datosIlustrador[0].nacionalidadIlustrador}</h4>`;
 
-		aux += `<div class="row">`; //abre fila karma
+	aux += `</div>`; //cierra fila nacionalidad
 
-		aux += `<button class="btn btn-success my-2 my-sm-0 margen-derecho-1 votakarma" onClick="${votaKarmaIlustrador(idIlustrador, (+1))}"><img src="../resources/img/iconos/glyphicons-344-thumbs-up.png" height="16px" alt="positivo"></button>`;
+	aux += `<div class="row">`; //abre fila karma
 
-		aux += `<button class="btn btn-danger my-2 my-sm-0 votakarma" onClick="${votaKarmaIlustrador(idIlustrador, (-1))}"><img src="../resources/img/iconos/glyphicons-345-thumbs-down.png" height="16px" alt="negativo"></button>`;
+	aux += `<button class="btn btn-success my-2 my-sm-0 margen-derecho-1 votakarma" onClick="${votaKarmaIlustrador(idIlustrador, (+1))}"><img src="../resources/img/iconos/glyphicons-344-thumbs-up.png" height="16px" alt="positivo"></button>`;
 
-		aux += `<span class="none texto-blanco margen-izquierdo-3" id="feedbackKarmaIlustrador">¡Gracias por votar!</span>`;
+	aux += `<button class="btn btn-danger my-2 my-sm-0 votakarma" onClick="${votaKarmaIlustrador(idIlustrador, (-1))}"><img src="../resources/img/iconos/glyphicons-345-thumbs-down.png" height="16px" alt="negativo"></button>`;
 
-		aux += `</div>`; //cierra fila karma
+	aux += `<span class="none texto-blanco margen-izquierdo-3" id="feedbackKarmaIlustrador">¡Gracias por votar!</span>`;
 
-		aux += `</div>`; //cierra columna datos
+	aux += `</div>`; //cierra fila karma
 
-		aux += `</div>`; //cierra fila total
+	aux += `</div>`; //cierra columna datos
 
-		aux += `<div class="row"><h1 class="col-lg-11 text-white center versalitas">Comics</h1></div>`;
+	aux += `</div>`; //cierra fila total
 
-		$('#datos').html(aux);
-	});
+	aux += `<div class="row"><h1 class="col-lg-11 text-white center versalitas">Comics</h1></div>`;
 
-	$.getJSON('comicsIlustrador.php?idIlustrador=' + idIlustrador, data => {
-		let cont = 0;
-		let aux = '';
+	$('#datos').html(aux);
 
-		aux += `<div class="row">`;//inicio div fila
+	let getComicsIlustrador = `SELECT DISTINCT portada, nombreComic, idComic FROM comic WHERE idIlustrador = '${idIlustrador}'`;
+	let comicsIlustrador = launchQuery(getComicsIlustrador);
+	let cont = 0;
+	let aux2 = '';
 
-		for (let i = 0; i < data.length; i++) {
-			if (cont == 3) {
-				aux += `</div>`; // cierra div row
-				aux += `<div class="row">`;//inicio de nuevo div fila
-				cont = 0; //resetea el contador
-			}
-			//aux += `<div class="col-sm-4">`; //dentro del div contenedor del item inicio un div columna
-			aux += `<div class="card bg-dark text-white" style="width: 20rem;">`; //dentro del div columna inicio un div card
+	aux2 += `<div class="row">`;//inicio div fila
 
-			aux += `<img src="../resources/img/portadas/${data[i].portada}" class="card-img-top" width="320px" height="500px">`; //imagen dentro del div item
-			aux += `<div class="card-block">`; // abre card-block
-			aux += `<h4 class="card-title">${data[i].nombreComic}</h4>`;
-			aux += `<a class="btn btn-primary align-items-end" href="#myModal" onClick="${modalDatosComic(data[i].idComic)}" data-toggle="modal" data-target=".bd-example-modal-lg">Ver detalles</a>`;
-
-			aux += `</div>`; //cierra div card-block
-			aux += `</div>`; //cierra div card
-			//aux += `</div>`; //cierra div columna
-			cont++;
+	for (let i = 0; i < comicsIlustrador.length; i++) {
+		if (cont == 3) {
+			aux2 += `</div>`; // cierra div row
+			aux2 += `<div class="row">`;//inicio de nuevo div fila
+			cont = 0; //resetea el contador
 		}
-		aux += `</div>`; //cierra div fila
 
-		$('#comics').html(aux);
-	});
+		aux2 += `<div class="card bg-dark text-white" style="width: 20rem;">`; //dentro del div columna inicio un div card
+
+		aux2 += `<img src="../resources/img/portadas/${comicsIlustrador[i].portada}" class="card-img-top" width="320px" height="500px">`; //imagen dentro del div item
+		aux2 += `<div class="card-block">`; // abre card-block
+		aux2 += `<h4 class="card-title">${comicsIlustrador[i].nombreComic}</h4>`;
+		aux2 += `<a class="btn btn-primary align-items-end" href="#myModal" onClick="${modalDatosComic(comicsIlustrador[i].idComic)}" data-toggle="modal" data-target=".bd-example-modal-lg">Ver detalles</a>`;
+
+		aux2 += `</div>`; //cierra div card-block
+		aux2 += `</div>`; //cierra div card
+
+		cont++;
+	}
+	aux2 += `</div>`; //cierra div fila
+
+	$('#comics').html(aux2);
 });
 
 let votaKarmaIlustrador = (idIlustrador, karma) => {
-	$.ajax({
-		type: 'POST',
-		url: '../php/votarKarmaIlustrador.php',
-		data: {
-			idIlustrador: idIlustrador,
-			karma: karma
-		},
-		success: data => {
-			$('.votakarma').prop('disabled', true);
-			$('#feedbackKarmaIlustrador').removeClass('none');
-			$('#feedbackKarmaIlustrador').addClass('block');
-		}
-	});
+	let updateKarmaIlustrador = `UPDATE ilustrador SET karmaIlustrador = karmaIlustrador + ${karma} WHERE idIlustrador = '${idIlustrador}'`;
+	launchQuery(updateKarmaIlustrador);
+
+	$('.votakarma').prop('disabled', true);
+	$('#feedbackKarmaIlustrador').removeClass('none');
+	$('#feedbackKarmaIlustrador').addClass('block');
 }
